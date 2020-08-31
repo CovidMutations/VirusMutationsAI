4f8db21c57490c7ac0839c9717f2de6b88e656da
a18 <andy1618@mail.ru>
2020-08-31 08:54:25 +0300

    Added unit-test for sample SnpEff file

--- backend/py/vcf_parser__test.py
+++ backend/py/vcf_parser__test.py
@@ -3,7 +3,8 @@ import unittest
 
 import vcf_parser
 
-TEST_VCF_FILE1 = 'test_data/total.vcf'
+TEST_VCF_FILE1 = 'db/sample.vcf'
+TEST_VCF_FILE2 = 'db/sample_snpeffect_top100.vcf'
 
 
 class TestVcfParser(unittest.TestCase):
@@ -18,7 +19,21 @@ class TestVcfParser(unittest.TestCase):
         print(muts.head())
 
         # Test write to file
-        output_file = f'__out_mutations_{time.time()}.txt'
+        output_file = f'__out_mutations1_{time.time()}.txt'
+        vcf_obj.write_mutations_to_file(output_file)
+
+
+    def test_load_with_snpeff_data(self):
+        vcf_obj = vcf_parser.VcfParser()
+        vcf_obj.read_vcf_file(TEST_VCF_FILE2)
+        self.assertEqual(vcf_obj.df_vcf.shape, (88, 10))
+
+        # Test get mutations
+        muts = vcf_obj.get_mutations()
+        print(muts.head())
+
+        # Test write to file
+        output_file = f'__out_mutations2_{time.time()}.txt'
         vcf_obj.write_mutations_to_file(output_file)
 
 
