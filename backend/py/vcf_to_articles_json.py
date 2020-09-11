26226ae42ebb121aa0be2462680bc6c3a625eea8
a18 <andy1618@mail.ru>
2020-09-11 12:58:42 +0300

    Type check in vcf_to_articles_json

--- backend/py/vcf_to_articles_json.py
+++ backend/py/vcf_to_articles_json.py
@@ -129,8 +129,9 @@ def main():
             vcf_parser_obj = vcf_parser.VcfParser()
             try:
                 vcf_parser_obj.read_vcf_file(args.vcf_file_or_request, verbose=verbose)
-                #vcf_mutations = vcf_parser_obj.get_mutations(verbose=verbose).tolist()
+                #vcf_mutations = vcf_parser_obj.get_mutations(verbose=verbose)
                 vcf_mutations = vcf_parser_obj.get_protein_mutations(is_strict_check=False, verbose=verbose)
+                assert isinstance(vcf_mutations, list)
             except Exception as e:
                 # Re-raise exception with additional context
                 raise Exception(f'ERROR while parsing vcf file: {e}')
