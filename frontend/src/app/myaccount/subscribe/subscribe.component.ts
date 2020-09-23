2871838938428f45e5c5275555bc5ccce65c72da
Natali Donova <natali.donova@auriga.com>
2020-09-23 17:00:44 +0300

    +

--- frontend/src/app/myaccount/subscribe/subscribe.component.ts
+++ frontend/src/app/myaccount/subscribe/subscribe.component.ts
@@ -11,13 +11,14 @@ import { ToolService } from '../../shared/tool.service';
 export class SubscribeComponent implements OnInit {
   dbUpdatingFrequency = DBUpdatingFrequencyEnum;
   subscribeForm: FormGroup;
+  mutations = new FormArray([]);
 
   constructor(
     readonly toolService: ToolService
   ) {
     this.subscribeForm = new FormGroup({
       frequency: new FormControl('',  Validators.required),
-      mutations: new FormArray([]) ,
+      mutations: this.mutations
     });
     this.addNewMutation();
 
@@ -27,11 +28,11 @@ export class SubscribeComponent implements OnInit {
   }
 
   addNewMutation(): void {
-    this.subscribeForm.get('mutations').push(new FormControl('', Validators.required));
+    this.mutations.push(new FormControl('', Validators.required));
   }
 
   onSubmit(): void {
-    console.log(this.subscribeForm.value)
+    console.log(this.subscribeForm.value);
     // this.authService.registration(this.subscribeForm.value).subscribe(data => {
     //   console.log('registration', data);
 
