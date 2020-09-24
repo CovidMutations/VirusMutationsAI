6213a9423c526a908bf271ba48d0609ad0796dfd
Natali Donova <natali.donova@auriga.com>
2020-09-24 14:19:52 +0300

    +

--- backend/src/mail/mail.service.ts
+++ backend/src/mail/mail.service.ts
@@ -1,71 +1,25 @@
 import { Injectable, Logger} from '@nestjs/common';
 import * as  nodemailer from 'nodemailer';
 import { MailModel} from '../model/mail.dto';
-import { google} from 'googleapis';
-const OAuth2 = google.auth.OAuth2;
 import * as config from 'config';
 const mailConfig = config.get('mail');
-const serverConfig = config.get('server');
+
 
 @Injectable()
 export class MailService {
   private logger = new Logger('MailService');
   userTokens = '';
-
-  oauth2Client = new OAuth2(
-    mailConfig.clientId,
-    mailConfig.clientSecret,
-    serverConfig.origin
-  );
   
-  // transporter = nodemailer.createTransport({
-  //   service: 'gmail',
-  //   auth: {
-  //     type: 'OAuth2',
-  //     clientId: mailConfig.clientId,
-  //     clientSecret: mailConfig.clientSecret,
-  //     user: mailConfig.email,
-  //   //  pass: mailConfig.pass
-  //   }
-  // });
+  transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: mailConfig.email,
+      pass: mailConfig.pass
+    }
+  });
 
   constructor() { 
-    this.googleAuth();
-  }
 
-  async googleAuth() {
-    this.logger.verbose(`googleAuth${JSON.stringify(this.oauth2Client)}`);
-    const accessToken = await this.oauth2Client.getAccessToken();
-
-    this.logger.verbose(`accessToken: ${JSON.stringify(accessToken)}`);
-    // this.logger.verbose(`refreshToken: ${JSON.stringify(refreshToken)}`);
-
-    // this.oauth2Client.setCredentials({
-    //   refresh_token: refreshToken
-    // });
-
-    
-    this.logger.verbose(`accessToken: ${JSON.stringify(accessToken)}`);
-
-    // this.transporter.set('oauth2_provision_cb', (user, renew, callback)=>{
-    //   this.logger.verbose(`oauth2_provision_cb: ${JSON.stringify(user)}`);
-
-    //   let accessToken = this.userTokens[user];
-    //   if(!accessToken){
-    //       return callback(new Error('Unknown user'));
-    //   }else{
-    //       return callback(null, accessToken);
-    //   }
-    // });
-    // this.transporter.on('token', token => {
-    //   this.logger.verbose(`transporter token: ${JSON.stringify(token)}`);
-
-    //   this.userTokens = token;
-    //   console.log('A new access token was generated');
-    //   console.log('User: %s', token.user);
-    //   console.log('Access Token: %s', token.accessToken);
-    //   console.log('Expires: %s', new Date(token.expires));
-    // });
   }
 
   send(mailOptions?: any) {
@@ -77,13 +31,13 @@ export class MailService {
     });
     console.log(_mailOptions)
 
-    // this.transporter.sendMail(_mailOptions, (error, info) =>{
-    //   if (error) {
-    //     this.logger.error(`${error}`);
-    //   } else {
-    //     this.logger.verbose(`Email sent: ${info.response}`);
-    //   }
-    // });
+    this.transporter.sendMail(_mailOptions, (error, info) =>{
+      if (error) {
+        this.logger.error(`${error}`);
+      } else {
+        this.logger.verbose(`Email sent: ${info.response}`);
+      }
+    });
   }
 
 }
