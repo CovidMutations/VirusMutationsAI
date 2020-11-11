04ee807b138299c7b05faef1a91df5041820dec2
Dmitry Malishev <dmitry.malishev@gmail.com>
2020-11-11 21:22:19 +0300

    Initial commit w/ user config (sudscr. interval)

--- backend2/src/api/api_v1/api.py
+++ backend2/src/api/api_v1/api.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from src.api.api_v1.endpoints import users
 from src.api.api_v1.endpoints import user_subscription
+from src.api.api_v1.endpoints import user_config
 
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/user")
 api_router.include_router(user_subscription.router, prefix="/user-subscription")
+api_router.include_router(user_config.router, prefix="/user-config")
