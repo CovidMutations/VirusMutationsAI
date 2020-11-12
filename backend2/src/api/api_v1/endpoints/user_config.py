2bed942c44ccc276d21872487da69e66cc64b1dd
Dmitry Malishev <dmitry.malishev@gmail.com>
2020-11-12 19:14:42 +0300

    Fixes on the code-review

--- backend2/src/api/api_v1/endpoints/user_config.py
+++ backend2/src/api/api_v1/endpoints/user_config.py
@@ -1,7 +1,10 @@
 from typing import Any
 
 from fastapi import APIRouter, Depends
+from sqlalchemy import func
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import concat
+from sqlalchemy.dialects.postgresql import INTERVAL
 
 from src.api import deps
 from src.db import models
@@ -9,23 +12,24 @@ from src.db import models
 router = APIRouter()
 
 
-@router.put("/config-me/subscription-interval/{value-days}", status_code=201)
+@router.put("/me/config/subscription-interval/{id}", status_code=201)
 def config_subscription_interval_user_me(
     *,
+    id: int,
     user: models.User = Depends(deps.get_current_active_user),
     db: Session = Depends(deps.get_db),
-    interval_days: int
 ) -> Any:
     """
     Set subscription interval
     """
-    interval = models.UserConfig(user_id=user.id, subscription_interval=interval_days)
+    interval = models.UserConfig(user_id=user.id, subscription_interval
+                                 =func.cast(concat(id, ' DAYS'), INTERVAL))
     db.merge(interval)
     db.commit()
 
-    return {"subscription_interval": interval_days}
+    return {"subscription_interval": id}
 
-@router.get("/config-me/subscription-interval", status_code=201)
+@router.get("/me/config/subscription-interval", status_code=201)
 def read_user_subscription_interval(
     user: models.User = Depends(deps.get_current_active_user),
     db: Session = Depends(deps.get_db),
