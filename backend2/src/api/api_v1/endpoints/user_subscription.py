cc9cb447c18762b87a3c6e4177b05d9f2f05f30d
Dmitry Malishev <dmitry.malishev@gmail.com>
2020-11-13 15:38:42 +0300

    Entity names fixed. Removed unused stuff.

--- backend2/src/api/api_v1/endpoints/user_subscription.py
+++ backend2/src/api/api_v1/endpoints/user_subscription.py
@@ -1,19 +1,17 @@
-from typing import Any, List
+from typing import Any
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from sqlalchemy import exc
 from psycopg2 import errors
 
-from src import schemas
 from src.api import deps
 from src.db import models
-from starlette.responses import Response
 
 router = APIRouter()
 
 
-@router.put("/subscribe/{mutation}", response_model=str, status_code=status.HTTP_201_CREATED)
+@router.put("/subscriptions/{mutation}", response_model=str, status_code=status.HTTP_201_CREATED)
 def subscribe_user_me(
     *,
     user: models.User = Depends(deps.get_current_active_user),
@@ -32,7 +30,7 @@ def subscribe_user_me(
         pass
     return mutation
 
-@router.get("/read-subscriptions", response_model=[])
+@router.get("/subscriptions", response_model=[])
 def read_subscriptions_user_me(
     skip: int = 0,
     limit: int = 100,
@@ -43,11 +41,9 @@ def read_subscriptions_user_me(
     Get subscription list
     """
     res = db.query(models.Subscription.mutation).filter(models.Subscription.user_id == user.id).slice(skip, limit).all()
-    if not res:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No subscriptions found")
     return [x[0] for x in res]
 
-@router.delete("/unsubscribe/{mutation}")
+@router.delete("/subscriptions/{mutation}")
 def unsubscribe_user_me(
     *,
     user: models.User = Depends(deps.get_current_active_user),
