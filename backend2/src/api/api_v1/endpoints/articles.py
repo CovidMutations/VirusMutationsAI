426fd6d91c621db94f007dfa1205f7e8e4f4e6af
Aleksey Maydokin <alexey.maydokin@auriga.com>
2020-12-07 20:18:57 +0300

    Search articles by VCF-file

--- backend2/src/api/api_v1/endpoints/articles.py
+++ backend2/src/api/api_v1/endpoints/articles.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends
+from collections import defaultdict
+
+from fastapi import APIRouter, Depends, UploadFile, File
 from sqlalchemy.orm import Session
 
 from src import schemas
 from src.api import deps
+from src.core.vcf_parser import VcfParser
 from src.db import models
 
 router = APIRouter()
@@ -19,10 +22,38 @@ def article_search(
     """
     mutation = item_in.mutation.upper()
     query = db.query(models.ArticleData).filter(models.ArticleData.mutations.any(mutation=mutation))
-    items = [schemas.ArticleSearchItem(
+    items = [article_data_to_search_item(item) for item in query.all()]
+
+    return {mutation: items} if items else {}
+
+
+@router.post("/search-by-file", response_model=schemas.ArticleSearchOut)
+def article_file_search(
+    *,
+    file: UploadFile = File(...),
+    db: Session = Depends(deps.get_db),
+) -> schemas.ArticleSearchOut:
+    """
+    Article search by mutations from a VCF-file
+    """
+    parser = VcfParser()
+    parser.read_vcf_file(file.file)
+    mutations = parser.get_mutations()
+
+    query = db.query(models.ArticleMutation.mutation, models.ArticleData)\
+        .join(models.ArticleMutation)\
+        .filter(models.ArticleMutation.mutation.in_(mutations))
+
+    res = defaultdict(list)
+    for mutation, item in query.all():
+        res[mutation].append(article_data_to_search_item(item))
+
+    return res
+
+
+def article_data_to_search_item(item: models.ArticleData) -> schemas.ArticleSearchItem:
+    return schemas.ArticleSearchItem(
         article_name=item.title,
         article_url=item.url,
         abstract_text=item.abstract_text,
-    ) for item in query.all()]
-
-    return {mutation: items} if items else {}
+    )
