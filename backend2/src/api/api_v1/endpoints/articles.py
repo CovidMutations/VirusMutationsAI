bc9440dbec93c0e1b1cd77f330918b1f5c156e4c
Aleksey Maydokin <alexey.maydokin@auriga.com>
2020-12-09 16:42:52 +0300

    Search articles by VCF-file with SNP effect

--- backend2/src/api/api_v1/endpoints/articles.py
+++ backend2/src/api/api_v1/endpoints/articles.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 
-from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import APIRouter, Depends, UploadFile, File, Body, HTTPException, status
+from pydantic import BaseModel
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from src import schemas
@@ -30,6 +32,7 @@ def article_search(
 @router.post("/search-by-file", response_model=schemas.ArticleSearchOut)
 def article_file_search(
     *,
+    use_snp_effect: bool = Body(False, alias="snpEffect"),
     file: UploadFile = File(...),
     db: Session = Depends(deps.get_db),
 ) -> schemas.ArticleSearchOut:
@@ -37,23 +40,59 @@ def article_file_search(
     Article search by mutations from a VCF-file
     """
     parser = VcfParser()
-    parser.read_vcf_file(file.file)
+    try:
+        parser.read_vcf_file(file.file)
+    except AssertionError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid VCF-file")
     mutations = parser.get_mutations()
 
-    query = db.query(models.ArticleMutation.mutation, models.ArticleData)\
-        .join(models.ArticleMutation)\
-        .filter(models.ArticleMutation.mutation.in_(mutations))
-
     res = defaultdict(list)
-    for mutation, item in query.all():
-        res[mutation].append(article_data_to_search_item(item))
+
+    if use_snp_effect:
+        query = db.query(
+            models.ArticleMutation.mutation,
+            models.MutationMapping.nucleotide,
+            models.ArticleData,
+        ) \
+            .join(models.ArticleMutation) \
+            .outerjoin(models.MutationMapping, models.MutationMapping.protein == models.ArticleMutation.mutation) \
+            .filter(or_(
+                models.ArticleMutation.mutation.in_(mutations),
+                models.MutationMapping.nucleotide.in_(mutations),
+            ))
+
+        for mutation, nucleotide, item in query.all():
+            if nucleotide:
+                key = nucleotide
+                base_mutation = BaseMutation(mutation=f"{nucleotide} => {mutation}", source="snpeff")
+            else:
+                key = mutation
+                base_mutation = None
+            res[key].append(article_data_to_search_item(item, base_mutation))
+    else:
+        query = db.query(models.ArticleMutation.mutation, models.ArticleData)\
+            .join(models.ArticleMutation)\
+            .filter(models.ArticleMutation.mutation.in_(mutations))
+
+        for mutation, item in query.all():
+            res[mutation].append(article_data_to_search_item(item))
 
     return res
 
 
-def article_data_to_search_item(item: models.ArticleData) -> schemas.ArticleSearchItem:
+class BaseMutation(BaseModel):
+    mutation: str
+    source: str
+
+
+def article_data_to_search_item(
+    item: models.ArticleData,
+    base_mutation: BaseMutation = None,
+) -> schemas.ArticleSearchItem:
     return schemas.ArticleSearchItem(
         article_name=item.title,
         article_url=item.url,
         abstract_text=item.abstract_text,
+        base_mutation=base_mutation.mutation if base_mutation else "",
+        base_mutation_src=base_mutation.source if base_mutation else None,
     )
